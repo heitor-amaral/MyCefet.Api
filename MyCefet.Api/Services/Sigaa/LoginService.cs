3d399844265e57044e04c0a37b7a4524e656f98c
Bruno H. A. Fonseca <bruno3145@hotmail.com>
2019-10-05 22:40:13 -0300

    Implementing a singleton strategy to treat request headers

--- MyCefet.Api/Services/Sigaa/LoginService.cs
+++ MyCefet.Api/Services/Sigaa/LoginService.cs
@@ -1,40 +1,28 @@
 ﻿using MyCefet.Api.Extensions;
 using MyCefet.Api.Interfaces;
 using MyCefet.Api.Services.Sigaa.Interfaces;
+using MyCefet.Api.Settings;
 using RestSharp;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MyCefet.Api.Services.Sigaa
 {
     public class LoginService : ILoginService
     {
-        private const string ACCEPT_ENC_HEADER_KEY = "Accept-Encoding";
-        private const string ACCEPT_ENC_HEADER_VALUE = "gzip, deflate, br";
-        private const string CACHE_HEADER_KEY = "Cache-Control";
-        private const string CACHE_HEADER_VALUE = "no-cache";
-        private const string REFERER_HEADER_KEY = "Referer";
-        private const string REFERER_HEADER_VALUE = "https://sig.cefetmg.br/sigaa/verPortalDiscente.do";
-        private const string COOKIE_HEADER_KEY = "Cookie";
-        private const string COOKIE_HEADER_VALUE = "JSESSIONID=";
-        private const string HOST_HEADER_KEY = "Host";
-        private const string HOST_HEADER_VALUE = "sig.cefetmg.br";
-        private const string CONNECTION_HEADER_KEY = "Connection";
-        private const string CONNECTION_HEADER_VALUE = "keep-alive";
-        private const string CONTENT_HEADER_KEY = "Content-Type";
-        private const string CONTENT_HEADER_VALUE = "application/x-www-form-urlencoded";
-        private const string ACCEPT_HEADER_KEY = "Accept";
-        private const string ACCEPT_HEADER_VALUE = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3";
-        private const string SIGAA_LOGON_URL = "https://sig.cefetmg.br/sigaa/logar.do?dispatch=logOn";
-        private const string SIGAA_BASE_URL = "https://sig.cefetmg.br/sigaa";
-        private const string LOGIN_PARAMETER_INIT = "width=800&height=600&urlRedirect=&subsistemaRedirect=&acao=&acessibilidade=&user.login=";
+
+        private readonly LoginRequestSettings _loginRequestSettings;
+
+        public LoginService(LoginRequestSettings loginRequestSettings)
+        {
+            _loginRequestSettings = loginRequestSettings;
+        }
 
         private async Task<string> GetJSession()
         {
             try
             {
-                var client = new RestClient(SIGAA_BASE_URL);
+                var client = new RestClient(_loginRequestSettings.SigaaBaseUrl);
                 var request = new RestRequest(Method.GET);
                 IRestResponse response = await client.ExecuteGetTaskAsync(request);
 
@@ -51,23 +39,12 @@ namespace MyCefet.Api.Services.Sigaa
             {
                 string jsession = await GetJSession();
 
-                var client = new RestClient(SIGAA_LOGON_URL);
+                var client = new RestClient(_loginRequestSettings.SigaaLogonUrl);
                 var request = new RestRequest(Method.POST);
 
-                var logonHeaders = new Dictionary<string, string>  
-                {
-                    {ACCEPT_ENC_HEADER_KEY, ACCEPT_ENC_HEADER_VALUE },
-                    {CACHE_HEADER_KEY, CACHE_HEADER_VALUE },
-                    {COOKIE_HEADER_KEY, COOKIE_HEADER_VALUE + jsession },
-                    { HOST_HEADER_KEY, HOST_HEADER_VALUE},
-                    {CONTENT_HEADER_KEY, CONTENT_HEADER_VALUE },
-                    {CONNECTION_HEADER_KEY, CONNECTION_HEADER_VALUE },
-                    {ACCEPT_HEADER_KEY, ACCEPT_HEADER_VALUE },
-                    { REFERER_HEADER_KEY,REFERER_HEADER_VALUE }
-                };
-                request.SetRequestHeaders(logonHeaders);
+                request.SetRequestHeaders(_loginRequestSettings.GetHeaderDict());
 
-                request.AddParameter("undefined", (LOGIN_PARAMETER_INIT + username + "&user.senha=" + password).Trim(), ParameterType.RequestBody);
+                request.AddParameter("undefined", (_loginRequestSettings.LoginParameterInit + username + "&user.senha=" + password).Trim(), ParameterType.RequestBody);
                 IRestResponse response = await client.ExecutePostTaskAsync(request);
 
                 Console.WriteLine(response.Content);
---
+++ MyCefet.Api/Settings/MySettings.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MyCefet.Api.Settings
+{
+
+    public partial class MySettings
+    {
+        [JsonProperty("Logging")]
+        public Logging Logging { get; set; }
+
+        [JsonProperty("AllowedHosts")]
+        public string AllowedHosts { get; set; }
+
+        [JsonProperty("Settings")]
+        public Settings Settings { get; set; }
+    }
+
+    public partial class ApiSettings
+    {
+        [JsonProperty("Identifier")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("AccessKey")]
+        public string AccessKey { get; set; }
+
+        [JsonProperty("Authorization")]
+        public string Authorization { get; set; }
+    }
+
+    public partial class Logging
+    {
+        [JsonProperty("LogLevel")]
+        public LogLevel LogLevel { get; set; }
+    }
+
+    public partial class LogLevel
+    {
+        [JsonProperty("Default")]
+        public string Default { get; set; }
+    }
+
+    public partial class Settings
+    {
+        [JsonProperty("RequestSettings")]
+        public RequestSettings RequestSettings { get; set; }
+
+        [JsonProperty("ApiSettings")]
+        public ApiSettings ApiSettings { get; set; }
+    }
+
+    public partial class RequestSettings
+    {
+        [JsonProperty("AcceptEncodeHeaderKey")]
+        public string AcceptEncodeHeaderKey { get; set; }
+
+        [JsonProperty("AcceptEncodeHeaderValue")]
+        public string AcceptEncodeHeaderValue { get; set; }
+
+        [JsonProperty("CacheHeaderKey")]
+        public string CacheHeaderKey { get; set; }
+
+        [JsonProperty("CacheHeaderValue")]
+        public string CacheHeaderValue { get; set; }
+
+        [JsonProperty("LoginCacheHeaderValue")]
+        public string LoginCacheHeaderValue { get; set; }
+
+        [JsonProperty("CookieHeaderKey")]
+        public string CookieHeaderKey { get; set; }
+
+        [JsonProperty("CookieHeaderValue")]
+        public string CookieHeaderValue { get; set; }
+
+        [JsonProperty("HostHeaderKey")]
+        public string HostHeaderKey { get; set; }
+
+        [JsonProperty("HostHeaderValue")]
+        public string HostHeaderValue { get; set; }
+
+        [JsonProperty("ContentHeaderKey")]
+        public string ContentHeaderKey { get; set; }
+
+        [JsonProperty("ContentHeaderValue")]
+        public string ContentHeaderValue { get; set; }
+
+        [JsonProperty("ConnectionHeaderKey")]
+        public string ConnectionHeaderKey { get; set; }
+
+        [JsonProperty("ConnectionHeaderValue")]
+        public string ConnectionHeaderValue { get; set; }
+
+        [JsonProperty("RefererHeaderKey")]
+        public string RefererHeaderKey { get; set; }
+
+        [JsonProperty("RefererHeaderValue")]
+        public string RefererHeaderValue { get; set; }
+
+        [JsonProperty("AcceptHeaderKey")]
+        public string AcceptHeaderKey { get; set; }
+
+        [JsonProperty("AcceptHeaderValue")]
+        public string AcceptHeaderValue { get; set; }
+
+        [JsonProperty("SigaaStudentPortalUrl")]
+        public string SigaaStudentPortalUrl { get; set; }
+
+        [JsonProperty("SigaaLogonUrl")]
+        public string SigaaLogonUrl { get; set; }
+
+        [JsonProperty("SigaaBaseUrl")]
+        public string SigaaBaseUrl { get; set; }
+
+        [JsonProperty("LoginParameterInit")]
+        public string LoginParameterInit { get; set; }
+    }
+
+    public abstract class BaseRequestSettings
+    {
+        public abstract Dictionary<string, string> GetHeaderDict();
+    }
+
+
+}
